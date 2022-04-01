0708cffe39fd21e3eb5819111b3203787dc0c848
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-04-01 13:29:13 +0530

    Update tap-install.sh

--- tap-install.sh
+++ tap-install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-
+yq '.buildservice.kp_default_repository_username' $HOME/tap-script-eks/tap-values.yaml > $dockerusername
+yq '.buildservice.kp_default_repository_password' $HOME/tap-script-eks/tap-values.yaml > $dockerpassword
+yq '.buildservice.tanzunet_username' $HOME/tap-script-eks/tap-values.yaml > $tanzunetusername
+yq '.buildservice.tanzunet_password' $HOME/tap-script-eks/tap-values.yaml > $tanzunet_password
+docker login $dockerhostname -u $dockerusername -p $dockerpassword
+docker login registry.tanzu.vmware.com -u $tanzunetusername -p $tanzunetpassword
+imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:1.0.2 --to-repo $dockerhostname/tap-demo/tap-packages
+tanzu secret registry add tap-registry --username $dockerusername --password $dockerpassword --server $dockerhostname --export-to-all-namespaces --yes --namespace tap-install
+tanzu package repository add tanzu-tap-repository --url $dockerhostname/tap-demo/tap-packages:1.0.2 --namespace tap-install
+tanzu package repository get tanzu-tap-repository --namespace tap-install
+tanzu package available list --namespace tap-install
 echo "############### TAP 1.0 Install   ##################"
 tanzu package install tap -p tap.tanzu.vmware.com -v 1.0.0 --values-file $HOME/tap-script/tap-values.yaml -n tap-install
 tanzu package installed list -A
